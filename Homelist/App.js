089b1c412899ae8897f26ee164e5c4d86b0f9bd3
Toretto00 <20521108@gm.uit.edu.vn>
2023-04-18 10:21:43 +0700

    Tabnavigation

--- Homelist/App.js
+++ Homelist/App.js
@@ -1,12 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import TabNavigator from './navigation/TabNavigator';
+
+import Home from './screens/Home';
+import Search from './screens/Search';
+import AdNew from './screens/AdNew';
+import Chats from './screens/Chats';
+import Account from './screens/Account';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Search' component={Search}/>
+        <Stack.Screen name='AdNew' component={AdNew}/>
+        <Stack.Screen name='Chats' component={Chats}/>
+        <Stack.Screen name='Account' component={Account}/>        
+      </Stack.Navigator> */}
+      <TabNavigator/>
+    </NavigationContainer>
   );
 }
 
---
+++ Homelist/navigation/TabNavigator.js
@@ -0,0 +1,54 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Home from '../screens/Home';
+import Search from '../screens/Search';
+import AdNew from '../screens/AdNew';
+import Chats from '../screens/Chats';
+import Account from '../screens/Account';
+import { COLORS } from '../variables/color';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+    return (
+        <Tab.Navigator 
+            initialRouteName='home'
+            screenOptions={
+                {headerShown: false}
+            }
+            tabBarOptions={{
+                activeTintColor: COLORS.primary,
+                activeTabStyle: {
+                    fontSize: 14,
+                    fontWeight: 'bold',
+                }
+            }}>
+            <Tab.Screen name='home' 
+                        component={Home}
+                        options={
+                            {tabBarIcon: ({color}) => (<MaterialCommunityIcons name="home" color={color} size={26} />)}                      
+                        }/>
+            <Tab.Screen name='search' 
+                        component={Search}
+                        options={
+                            {tabBarIcon: ({color}) => (<MaterialCommunityIcons name="magnify" color={color} size={26} />)}                        
+                        }/>
+            <Tab.Screen name='adnew' 
+                        component={AdNew}
+                        options={
+                            {tabBarIcon: ({color}) => (<MaterialCommunityIcons name="home-plus" color={color} size={26} />)}                        
+                        }/>
+            <Tab.Screen name='chats' 
+                        component={Chats}
+                        options={
+                            {tabBarIcon: ({color}) => (<MaterialCommunityIcons name="chat" color={color} size={26} />)}                        
+                        }/>
+            <Tab.Screen name='account' 
+                        component={Account}
+                        options={
+                            {tabBarIcon: ({color}) => (<MaterialCommunityIcons name="account" color={color} size={26} />)}                        
+                        }/>
+        </Tab.Navigator>
+    );
+}
