4bb7626e34ba88bd66bef53e74dd7ce32cfcc73d
Toretto00 <20521108@gm.uit.edu.vn>
2023-05-15 22:53:42 +0700

    Listing detail screen

--- Homelist/screens/Home.jsx
+++ Homelist/screens/Home.jsx
@@ -1,23 +1,41 @@
-import { useCallback, useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground, FlatList, SafeAreaView, Dimensions } from 'react-native';
-import { Button, Divider, TextInput, Avatar, ToggleButton, Card } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useCallback, useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  FlatList,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+import {
+  Button,
+  Divider,
+  TextInput,
+  Avatar,
+  ToggleButton,
+  Card,
+} from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { decode }  from  "html-entities";
+import { decode } from "html-entities";
 
-import { COLORS } from '../variables/color';
-import Header from '../components/Header';
-import api from '../api/client';
-import CategoryIcon from '../components/CategoryIcon';
+import { COLORS } from "../variables/color";
+import Header from "../components/Header";
+import api from "../api/client";
+import CategoryIcon from "../components/CategoryIcon";
 
-const {height: screenHeight, width: screenWidth} = Dimensions.get('screen');
+const { height: screenHeight, width: screenWidth } = Dimensions.get("screen");
 
-export default function Home() {
+export default function Home({ navigation }) {
   const [isSearch, setIsSearch] = useState(false);
-  const [isCheck, setIsChech] = useState('square');
-  const [searchInput, setSearchInput] = useState('');
+  const [isCheck, setIsChech] = useState("square");
+  const [searchInput, setSearchInput] = useState("");
   const [listingsData, setListingsData] = useState([]);
   const [categoriesData, setCategoriesData] = useState([]);
   const [initial, setInitial] = useState(true);
@@ -27,181 +45,278 @@ export default function Home() {
   }, [searchInput]);
 
   useEffect(() => {
-    if(!initial) return;
+    if (!initial) return;
     handleLoadListingsData();
   }, [initial]);
 
   useEffect(() => {
-    if(!initial) return;
+    if (!initial) return;
     handleLoadCategoriesData();
   }, [initial]);
 
-  function handleLoadListingsData(){
-    api.get('listings').then((res) => {
-      if(res.ok)
-      {
-        setListingsData(res.data.data);
-      }
-      if(initial) setInitial(false);
-    }).catch((error) => {
-      console.log(error);
-    })
-  };
+  function handleLoadListingsData() {
+    api
+      .get("listings")
+      .then((res) => {
+        if (res.ok) {
+          setListingsData(res.data.data);
+        }
+        if (initial) setInitial(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-  function handleLoadCategoriesData(){
-    api.get('categories').then((res) => {
-      if(res.ok)
-      {
-        setCategoriesData(res.data);
-      }
-    }).catch((error) => {
-      console.log(error);
-    })
-  };
+  function handleLoadCategoriesData() {
+    api
+      .get("categories")
+      .then((res) => {
+        if (res.ok) {
+          setCategoriesData(res.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-  function handleSearchInput(){
-    if(searchInput !== '')
-      setIsSearch(true);
-    else
-      setIsSearch(false);
-  };
+  function handleSearchInput() {
+    if (searchInput !== "") setIsSearch(true);
+    else setIsSearch(false);
+  }
 
-  const renderCategoriesList = useCallback(
-    ({item}) => (
-      <TouchableOpacity style={styles.categoriesBtn}>
-        <TouchableOpacity style={styles.categoriesBtnImage}>
-          {/* <Image style={styles.image} source={require('../assets/apartment.png')}/> */}
-          <CategoryIcon iconName={item.icon.class} iconColor={COLORS.primary} iconSize={18}/>
-        </TouchableOpacity>
-        <Text style={styles.textGray}>{decode(item.name)}</Text>
-      </TouchableOpacity>
-    )
-  );
+  function handleListingDetail(listingId) {
+    navigation.navigate("Listing Detail", { listingId });
+  }
 
-  const renderItemList = useCallback(
-    ({item}) => (
-      <Card style={styles.cardContainer}>
-        <Card.Cover source={item?.images?.length
-                  ? {
-                      uri: item.images[0].sizes.full.src,
-                    }
-                  : require('../assets/icon.png')}/>
-        <Card.Content style={styles.margin8}>
-          <Button mode='Contained'
-                  textColor={COLORS.primary}
-                  style={{backgroundColor: COLORS.bg_primary, justifyContent: 'flex-start', borderRadius: 4}}>
-            {decode(item.categories[0].name)}
-          </Button>
-        </Card.Content>
-        <Card.Title title={decode(item.title)}
-                    titleNumberOfLines={2}
-                    titleStyle={styles.bold}
+  const renderCategoriesList = useCallback(({ item }) => (
+    <TouchableOpacity style={styles.categoriesBtn}>
+      <TouchableOpacity style={styles.categoriesBtnImage}>
+        <CategoryIcon
+          iconName={item.icon.class}
+          iconColor={COLORS.primary}
+          iconSize={18}
         />
-        <Card.Content style={{flexDirection: 'row', alignItems: 'center', marginTop: 4}}>
-          <Image source={require('../assets/pin.png')}/>
-          <Text style={styles.textGray}>{item.contact.locations.map((_loc)=>_loc.name).join(", ")}</Text>
-        </Card.Content>
-        <Card.Content style={{marginTop: 4}}>
-          <Text style={{fontWeight: 'bold', fontSize: 18}}>{decode(item.price)}$</Text>
-        </Card.Content>
-      </Card>
+      </TouchableOpacity>
+      <Text style={styles.textGray}>{decode(item.name)}</Text>
+    </TouchableOpacity>
   ));
 
-  const renderItemTextList = useCallback(
-    ({item}) => (
-      <Card style={styles.cardTextContainer} onPress={() => console.log(item.custom_fields[0].options.choices.map((name)=>name.name).join('/'))}>
-        <View style={styles.cardTextContainer}>
-          <Card.Cover source={item?.images?.length
-                    ? {
-                        uri: item.images[0].sizes.full.src,
-                      }
-                    : require('../assets/icon.png')}
-                      style={{width: (screenWidth - 16) / 5 * 2}}
-                    />
-          <View style={{width: (screenWidth - 16) / 5 * 3}}>
-            <Card.Content style={styles.margin8}>
-              <Button mode='Contained'
-                      textColor={COLORS.primary}
-                      style={{backgroundColor: COLORS.bg_primary, justifyContent: 'flex-start', borderRadius: 4}}>
-                {decode(item.categories[0].name)}
-              </Button>
-            </Card.Content>
-            <Card.Title title={decode(item.title)}
-                        titleNumberOfLines={2}
-                        titleStyle={styles.bold}
-            />
-            <Card.Content style={{flexDirection: 'row', alignItems: 'center', marginTop: 4}}>
-              <Image source={require('../assets/pin.png')}/>
-              <Text style={styles.textGray}>{item.contact.locations.map((_loc)=>_loc.name).join(", ")}</Text>
-            </Card.Content>
-            <Card.Content style={{marginTop: 4, alignItems: 'flex-end'}}>
-              <Text style={{fontWeight: 'bold', fontSize: 18}}>{decode(item.price)}$</Text>
-            </Card.Content>
-            <Card.Content>
-              <Text style={styles.textGray} numberOfLines={2}>{item.custom_fields[0].options.choices.map((name)=>name.name).join('/ ')}</Text>
-            </Card.Content>
-          </View>
-        </View>        
-      </Card>
+  const renderItemList = useCallback(({ item }) => (
+    <Card
+      style={styles.cardContainer}
+      onPress={() => handleListingDetail(item.listing_id)}
+    >
+      <Card.Cover
+        source={
+          item?.images?.length
+            ? {
+                uri: item.images[0].sizes.full.src,
+              }
+            : require("../assets/icon.png")
+        }
+      />
+      <Card.Content style={styles.margin8}>
+        <Button
+          mode="Contained"
+          textColor={COLORS.primary}
+          style={{
+            backgroundColor: COLORS.bg_primary,
+            justifyContent: "flex-start",
+            borderRadius: 4,
+          }}
+        >
+          {decode(item.categories[0].name)}
+        </Button>
+      </Card.Content>
+      <Card.Title
+        title={decode(item.title)}
+        titleNumberOfLines={2}
+        titleStyle={styles.bold}
+      />
+      <Card.Content
+        style={{ flexDirection: "row", alignItems: "center", marginTop: 4 }}
+      >
+        <Image source={require("../assets/pin.png")} />
+        <Text style={styles.textGray}>
+          {item.contact.locations.map((_loc) => _loc.name).join(", ")}
+        </Text>
+      </Card.Content>
+      <Card.Content style={{ marginTop: 4 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+          {decode(item.price)}$
+        </Text>
+      </Card.Content>
+    </Card>
+  ));
+
+  const renderItemTextList = useCallback(({ item }) => (
+    <Card
+      style={styles.cardTextContainer}
+      onPress={() => handleListingDetail()}
+    >
+      <View style={styles.cardTextContainer}>
+        <Card.Cover
+          source={
+            item?.images?.length
+              ? {
+                  uri: item.images[0].sizes.full.src,
+                }
+              : require("../assets/icon.png")
+          }
+          style={{ width: ((screenWidth - 16) / 5) * 2 }}
+        />
+        <View style={{ width: ((screenWidth - 16) / 5) * 3 }}>
+          <Card.Content style={styles.margin8}>
+            <Button
+              mode="Contained"
+              textColor={COLORS.primary}
+              style={{
+                backgroundColor: COLORS.bg_primary,
+                justifyContent: "flex-start",
+                borderRadius: 4,
+              }}
+            >
+              {decode(item.categories[0].name)}
+            </Button>
+          </Card.Content>
+          <Card.Title
+            title={decode(item.title)}
+            titleNumberOfLines={2}
+            titleStyle={styles.bold}
+          />
+          <Card.Content
+            style={{ flexDirection: "row", alignItems: "center", marginTop: 4 }}
+          >
+            <Image source={require("../assets/pin.png")} />
+            <Text style={styles.textGray}>
+              {item.contact.locations.map((_loc) => _loc.name).join(", ")}
+            </Text>
+          </Card.Content>
+          <Card.Content style={{ marginTop: 4, alignItems: "flex-end" }}>
+            <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+              {decode(item.price)}$
+            </Text>
+          </Card.Content>
+          <Card.Content>
+            <Text style={styles.textGray} numberOfLines={2}>
+              {item.custom_fields[0].options.choices
+                .map((name) => name.name)
+                .join("/ ")}
+            </Text>
+          </Card.Content>
+        </View>
+      </View>
+    </Card>
   ));
 
   return (
     <View style={styles.container}>
-      <Header/>
+      <Header />
       <View style={styles.homeAction}>
-        <Button mode='outlined'
-                textColor={COLORS.black}
-                style={styles.locationBtn}>
-          <Image source={require('../assets/pin.png')}/>
+        <Button
+          mode="outlined"
+          textColor={COLORS.black}
+          style={styles.locationBtn}
+        >
+          <Image source={require("../assets/pin.png")} />
           <Text>Location</Text>
         </Button>
-        <TextInput mode='outlined'
-                    placeholder='Search...'
-                    activeOutlineColor={COLORS.primary}
-                    left={<TextInput.Icon icon={() => <Icon name='magnify' color={COLORS.primary} size={24} style={styles.margin8}/>}/>}
-                    right={isSearch ? <TextInput.Icon icon={() => <Icon name='close' color={COLORS.primary} size={24} style={styles.margin8}/>} onPress={() => setSearchInput('')}/> : <></>}
-                    style={[styles.transparentColor, styles.searchInput]}
-                    value={searchInput}
-                    onChangeText={(value) => {setSearchInput(value)}}
+        <TextInput
+          mode="outlined"
+          placeholder="Search..."
+          activeOutlineColor={COLORS.primary}
+          left={
+            <TextInput.Icon
+              icon={() => (
+                <Icon
+                  name="magnify"
+                  color={COLORS.primary}
+                  size={24}
+                  style={styles.margin8}
+                />
+              )}
+            />
+          }
+          right={
+            isSearch ? (
+              <TextInput.Icon
+                icon={() => (
+                  <Icon
+                    name="close"
+                    color={COLORS.primary}
+                    size={24}
+                    style={styles.margin8}
+                  />
+                )}
+                onPress={() => setSearchInput("")}
+              />
+            ) : (
+              <></>
+            )
+          }
+          style={[styles.transparentColor, styles.searchInput]}
+          value={searchInput}
+          onChangeText={(value) => {
+            setSearchInput(value);
+          }}
         />
       </View>
-      <Divider/>
+      <Divider />
       <View style={styles.categoriesTitle}>
         <Text style={styles.bold}>Top Categories</Text>
-        <Button mode='text'
-                textColor={COLORS.primary}>
+        <Button mode="text" textColor={COLORS.primary}>
           See all
         </Button>
       </View>
-      <SafeAreaView style={{height: 100,}}>
-        <FlatList data={categoriesData} 
-                  renderItem={renderCategoriesList} 
-                  horizontal={true} 
-                  showsHorizontalScrollIndicator={false}/>
+      <SafeAreaView style={{ height: 100 }}>
+        <FlatList
+          data={categoriesData}
+          renderItem={renderCategoriesList}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
       </SafeAreaView>
       <View style={styles.adsContent}>
         <Text style={styles.bold}>Lastest Ads</Text>
-        <View style={{flexDirection:'row',}}>
-          <ToggleButton.Group onValueChange={(value) => setIsChech(value)}
-                              value={isCheck}>
-            <ToggleButton icon='square' value='square' iconColor={COLORS.primary}></ToggleButton>
-            <ToggleButton icon='format-list-text' value='text' iconColor={COLORS.primary}></ToggleButton>
+        <View style={{ flexDirection: "row" }}>
+          <ToggleButton.Group
+            onValueChange={(value) => setIsChech(value)}
+            value={isCheck}
+          >
+            <ToggleButton
+              icon="square"
+              value="square"
+              iconColor={COLORS.primary}
+            ></ToggleButton>
+            <ToggleButton
+              icon="format-list-text"
+              value="text"
+              iconColor={COLORS.primary}
+            ></ToggleButton>
           </ToggleButton.Group>
         </View>
       </View>
-      <SafeAreaView style={styles.container}>        
-        {isCheck === 'square' ? 
-          <FlatList data={listingsData}
-                    key={'_'}
-                    horizontal={false}
-                    numColumns={2}
-                    renderItem={ renderItemList }/> : 
-          <FlatList data={listingsData}
-                    key={'#'}
-                    horizontal={false}
-                    numColumns={1}
-                    renderItem={ renderItemTextList }/>}
-      </SafeAreaView>      
+      <SafeAreaView style={styles.container}>
+        {isCheck === "square" ? (
+          <FlatList
+            data={listingsData}
+            key={"_"}
+            horizontal={false}
+            numColumns={2}
+            renderItem={renderItemList}
+          />
+        ) : (
+          <FlatList
+            data={listingsData}
+            key={"#"}
+            horizontal={false}
+            numColumns={1}
+            renderItem={renderItemTextList}
+          />
+        )}
+      </SafeAreaView>
     </View>
   );
 }
@@ -209,17 +324,17 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   homeAction: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
     padding: 8,
   },
   locationBtn: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     height: 46,
     borderRadius: 2,
     marginRight: 8,
@@ -227,9 +342,9 @@ const styles = StyleSheet.create({
   searchInput: {
     height: 46,
     width: 260,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
-  transparentColor:{
+  transparentColor: {
     backgroundColor: COLORS.transparent,
     height: 60,
   },
@@ -237,20 +352,20 @@ const styles = StyleSheet.create({
     marginTop: 8,
   },
   categoriesTitle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginLeft: 8,
   },
   bold: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   categoriesBtn: {
     height: 80,
     width: 80,
     margin: 8,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   categoriesBtnImage: {
     padding: 16,
@@ -266,9 +381,9 @@ const styles = StyleSheet.create({
   },
   adsContent: {
     padding: 8,
-    flexDirection:'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   cardContainer: {
     width: screenWidth / 2 - 16,
@@ -276,9 +391,9 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.white,
   },
   cardTextContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     width: screenWidth - 16,
     margin: 8,
     backgroundColor: COLORS.white,
-  }
+  },
 });
---
+++ Homelist/screens/ListingDetail.jsx
@@ -0,0 +1,284 @@
+import { useCallback, useEffect, useState } from "react";
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+import { decode } from "html-entities";
+
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+import {
+  Button,
+  IconButton,
+  Card,
+  ActivityIndicator,
+} from "react-native-paper";
+
+import { ImageSlider } from "react-native-image-slider-banner";
+
+import { COLORS } from "../variables/color";
+import api from "../api/client";
+import CategoryIcon from "../components/CategoryIcon";
+
+const { height: screenHeight, width: screenWidth } = Dimensions.get("screen");
+
+export default function ListingDetail({ route, navigation }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isExpand, setIsExpand] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [listingData, setListingData] = useState();
+  const [initial, setInitial] = useState(true);
+
+  const listImages = [];
+  const amenities = [];
+  const customField = [];
+
+  useEffect(() => {
+    handleLoadListingData();
+  }, []);
+
+  useEffect(() => {
+    handleLoadImages();
+  });
+
+  useEffect(() => {
+    getCustomField();
+    setTimeout(() => setIsLoading(false), 1000);
+  }, []);
+
+  function handleLoadListingData() {
+    api.get(`/listings/${route.params.listingId}`).then((res) => {
+      if (res.ok) {
+        setListingData(res.data);
+      }
+    });
+  }
+
+  function handleLoadImages() {
+    listingData?.images?.map((image) => {
+      listImages.push({ img: image.sizes.full.src });
+    });
+  }
+
+  const getLocation = (contact) => {
+    const address = [];
+    if (contact?.locations[0]) address.push(contact.locations[0].name);
+    return address.join(", ");
+  };
+
+  function getCustomField() {
+    if (listingData?.custom_fields) {
+      listingData.custom_fields.map((item1) => {
+        if (item1.label !== "Amenities" && item1.value) {
+          customField.push(item1);
+        }
+      });
+    }
+    console.log(customField);
+    return customField;
+  }
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator
+          color={COLORS.primary}
+          style={{ justifyContent: "center", alignItems: "center", flex: 1 }}
+          size="large"
+        />
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <IconButton
+              icon="arrow-left"
+              containerColor={COLORS.white}
+              onPress={() => navigation.pop()}
+            />
+            <View style={{ flexDirection: "row" }}>
+              <IconButton icon="share-variant" containerColor={COLORS.white} />
+              <IconButton
+                icon={isFavorite ? "cards-heart" : "cards-heart-outline"}
+                containerColor={COLORS.white}
+                onPress={() => setIsFavorite(!isFavorite)}
+              />
+            </View>
+          </View>
+          <ScrollView>
+            <ImageSlider data={listImages} autoPlay={true} timer={1000} />
+            <Text style={styles.listingTitle}>{listingData?.title}</Text>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", margin: 8 }}
+            >
+              <Image source={require("../assets/pin.png")} />
+              <Text style={[styles.textGray, { marginLeft: 10 }]}>
+                {getLocation(listingData?.contact)}
+              </Text>
+            </View>
+            <Text style={styles.priceText}>${listingData?.price}</Text>
+            <Text style={[styles.priceType, styles.textGray]}>
+              {listingData?.price_type}
+            </Text>
+            <Card mode="outlined" style={{ margin: 8 , backgroundColor: COLORS.white}}>
+              <Card.Content>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    //margin: 8,
+                  }}
+                >
+                  {getCustomField().map((field) => (
+                    <View key={decode(field.id)}>
+                      <View style={styles.categoriesBtn}>
+                        <View style={styles.categoriesBtnImage}>
+                          <CategoryIcon
+                            iconName={
+                              field.icon.trim().includes("flaticon-")
+                                ? field.icon.trim().slice(9)
+                                : field.icon
+                            }
+                            iconColor={COLORS.primary}
+                            iconSize={18}
+                          />
+                        </View>
+                        <Text style={{ fontWeight: "bold" }}>
+                          {decode(field.label)}
+                        </Text>
+                        <Text style={styles.textGray}>
+                          {decode(field.value)}
+                        </Text>
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              </Card.Content>
+            </Card>
+
+            <Text style={{ fontWeight: "bold", fontSize: 16, margin: 8 }}>
+              {listingData?.custom_fields[0].label}
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                margin: 8,
+              }}
+            >
+              {listingData?.custom_fields[0].options.choices.map((item) => (
+                <View key={item.id}>
+                  <View
+                    style={[
+                      {
+                        flexDirection: "row",
+                        width: (screenWidth - 16) / 2,
+                        alignItems: "center",
+                      },
+                    ]}
+                  >
+                    <FontAwesome name="check" color={COLORS.primary} />
+                    <Text style={[styles.textGray, { marginLeft: 4 }]}>
+                      {item.name}
+                    </Text>
+                  </View>
+                </View>
+              ))}
+            </View>
+            <Text style={{ fontWeight: "bold", fontSize: 16, margin: 8 }}>
+              Description
+            </Text>
+            <Card style={{backgroundColor: COLORS.bg_light}}>
+              <Card.Content>
+                {!isExpand ? (
+                  <Text>{listingData?.description.slice(0, 100)}...</Text>
+                ) : (
+                  <Text>{listingData?.description}</Text>
+                )}
+                <Button
+                  mode="text"
+                  textColor={COLORS.primary}
+                  onPress={() => setIsExpand(!isExpand)}
+                >
+                  {!isExpand ? "Show more" : "Show less"}
+                </Button>
+              </Card.Content>
+            </Card>
+          </ScrollView>
+          <View style={{ flexDirection: "row" }}>
+            <Button
+              mode="contained-tonal"
+              icon="phone"
+              style={{ width: screenWidth / 3 }}
+            >
+              Call
+            </Button>
+            <Button
+              mode="contained-tonal"
+              icon="email"
+              style={{ width: screenWidth / 3 }}
+            >
+              Email
+            </Button>
+            <Button
+              mode="contained-tonal"
+              icon="chat"
+              style={{ width: screenWidth / 3 }}
+            >
+              Chat
+            </Button>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    height: 100,
+    backgroundColor: COLORS.primary,
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+  },
+  listingTitle: {
+    fontWeight: "bold",
+    fontSize: 16,
+    margin: 8,
+  },
+  priceText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    margin: 8,
+  },
+  priceType: {
+    margin: 8,
+  },
+  textGray: {
+    color: COLORS.text_gray,
+  },
+  categoriesBtn: {
+    height: 80,
+    width: (screenWidth - 32 * 4) / 4,
+    margin: 8,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  categoriesBtnImage: {
+    padding: 16,
+    borderRadius: 40,
+    backgroundColor: COLORS.bg_primary,
+  },
+});
